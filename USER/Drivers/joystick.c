17fe948ca060b64d2695e35ea549da9a57179232
LeehotYuu <1472973494@qq.com>
2023-08-16 10:09:31 +0800

    The HID send data function in joystick.c file is replaced with the CDC send data function
    USBD_CUSTOM_HID_SendReport都更换为CDC_Transmit_FS

--- USER/Drivers/joystick.c
+++ USER/Drivers/joystick.c
@@ -48,7 +48,7 @@ void joystickTask(void *param)
                 checkSum += hidReportData[i]&0x00FF;
             }
             hidReportData[7] = checkSum; 
-            USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
+            CDC_Transmit_FS((uint8_t*) &hidReportData, 8*sizeof(uint16_t));
         }
         else if (requestType1 == REQUEST_CONIFG_INFO)
         {
@@ -64,7 +64,7 @@ void joystickTask(void *param)
                     checkSum += hidReportData[i]&0x00FF;
                 }
                 hidReportData[7] = checkSum;
-                USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
+                CDC_Transmit_FS((uint8_t*) &hidReportData, 8*sizeof(uint16_t));
             }
             else if(requestType2 == 0x01)/*internal_info*/
             {
@@ -82,7 +82,7 @@ void joystickTask(void *param)
                     checkSum += hidReportData[i] & 0x00FF;
                 }
                 hidReportData[7] = checkSum;
-                USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
+                CDC_Transmit_FS((uint8_t*) &hidReportData, 8*sizeof(uint16_t));
 
             }
             else if(requestType2 == 0x02)/*external_info*/
@@ -181,7 +181,7 @@ void joystickTask(void *param)
                         checkSum += hidReportData[i]&0x00FF;
                     }
                     hidReportData[7] = checkSum;
-                    USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
+                    CDC_Transmit_FS((uint8_t*) &hidReportData, 8*sizeof(uint16_t));
                 }
                 
             }
@@ -217,7 +217,7 @@ void joystickTask(void *param)
                 checkSum += hidReportData[i]&0x00FF;
             }
             hidReportData[7] = checkSum;
-            USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
+            CDC_Transmit_FS((uint8_t*) &hidReportData, 8*sizeof(uint16_t));
 
         }
         else if(requestType1 == REQUEST_EXTRA_CONFIG_INFO)
@@ -233,7 +233,7 @@ void joystickTask(void *param)
                 checkSum += hidReportData[i]&0x00FF;
             }
             hidReportData[7] = checkSum;
-            USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
+            CDC_Transmit_FS((uint8_t*) &hidReportData, 8*sizeof(uint16_t));
         }
         else 
         {
@@ -245,7 +245,7 @@ void joystickTask(void *param)
             hidReportData[5] = map(mixValBuff[5],988,2012,0,2047);
             hidReportData[6] = map(mixValBuff[6],988,2012,0,2047);
             hidReportData[7] = map(mixValBuff[7],988,2012,0,2047);
-            USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
+            CDC_Transmit_FS((uint8_t*) &hidReportData, 8*sizeof(uint16_t));
         }
         checkSum = 0;        
 
